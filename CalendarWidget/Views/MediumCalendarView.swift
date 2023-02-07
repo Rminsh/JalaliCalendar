000d08a297f34656009ea2ad3ddca8b58b85f994
Armin <armin536@gmail.com>
2023-02-08 01:41:51 +0330

    Improved MonthView calendar content in Medium widget view

--- CalendarWidget/Views/MediumCalendarView.swift
+++ CalendarWidget/Views/MediumCalendarView.swift
@@ -24,30 +24,33 @@ struct MediumCalendarView: View {
                 month: date,
                 showHeader: false
             ) { weekday in
-                Text("30")
-                    .hidden()
-                    .padding(8)
-                    .clipShape(Circle())
-                    .padding(.vertical, 2)
-                    .overlay(
-                        Text(weekday)
-                            .customFont(name: "Shabnam", style: .caption2)
-                    )
-                    .frame(minWidth: 0, maxHeight: 15)
+                Text(weekday)
+                    .customFont(style: .caption2)
+                    .minimumScaleFactor(0.8)
+                    .lineLimit(1)
+                    .dynamicTypeSize(.xSmall)
+                    .frame(maxWidth: .infinity)
                     .padding(.bottom, 2)
             } content: { dateItem in
-                Text("30")
-                    .hidden()
-                    .padding(8)
-                    .background(dateItem.checkIsToday(date: date) ? Color("AccentColor"): .clear)
-                    .clipShape(Circle())
-                    .padding(.vertical, 2)
-                    .overlay(
-                        Text(JalaliHelper.DayFa.string(from: dateItem))
-                            .customFont(name: "Shabnam", style: .caption2)
-                            .foregroundColor(dateItem.checkIsToday(date: date) ? Color.white: Color("TextColor"))
+                Text(JalaliHelper.DayFa.string(from: dateItem))
+                    .customFont(style: .body)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.56)
+                    .dynamicTypeSize(.xSmall ... .small)
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(
+                        dateItem.checkIsToday(date: date) ?
+                        Color.white:
+                        Color("TextColor")
                     )
-                    .frame(minWidth: 0, maxHeight: 15)
+                    .background(
+                        Circle().fill(
+                            dateItem.checkIsToday(date: date) ?
+                            Color("AccentColor"):
+                                Color.clear
+                            )
+                        .padding(-2)
+                    )
             }
             .environment(\.layoutDirection, .rightToLeft)
             .environment(\.calendar, .persianCalendar)
