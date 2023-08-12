a5725c36b5a633a90abcb7e31eea0788443536f9
Armin <armin536@gmail.com>
2023-08-12 12:49:58 +0330

    fixed a warning for widgets

--- Widget/TodayCalendar/RectangularCalendarView.swift
+++ Widget/TodayCalendar/RectangularCalendarView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import WidgetKit
 
-@available(iOSApplicationExtension, introduced: 16.0)
+@available(iOS, introduced: 16.0)
 struct RectangularCalendarView: View {
     
     var date: Date
